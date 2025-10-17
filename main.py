5d1a66730cc246be216b4b91b0c84c8ce5e7d75b
RandyWuPro <randy.b.wu@gmail.com>
2025-10-17 11:28:22 -0700

    Added feature to allow user to enter booktext file they want to run a report.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_num_char, sort_on, sort_char_counts
 
 # Function to retrieve the filepath and read the content within the file
@@ -8,34 +9,40 @@ def get_book_text(filepath):
         return file_contents
             
 def main():
-    filepath = "books/frankenstein.txt"
-    retrieved_content = get_book_text(filepath)
-    
-    # word count
-    num_words = get_num_words(retrieved_content)
-    
-    # Character counts in dictionary
-    char_counts = get_num_char(retrieved_content)
-    
-    # Sort charcter counts by frequency
-    sorted_chars = sort_char_counts(char_counts)
-    
-    # Print Bookbot title
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {filepath}...")
-    
-    # Print Word Count
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    
-    # Print character frequency
-    print("--------- Character Count -------")
-    for item in sorted_chars:
-        char = item["char"]
-        count = item["num"]
-        if char.isalpha():
-            print(f"{char}: {count}")
-    
-    # Print End
-    print("============= END ===============")
+    # Checks if sys.argv have two entries or not. Provide guidance if not.
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    else:   
+        filepath = sys.argv[1]
+        retrieved_content = get_book_text(filepath)
+        
+        # word count
+        num_words = get_num_words(retrieved_content)
+        
+        # Character counts in dictionary
+        char_counts = get_num_char(retrieved_content)
+        
+        # Sort charcter counts by frequency
+        sorted_chars = sort_char_counts(char_counts)
+        
+        # Print Bookbot title
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {filepath}...")
+        
+        # Print Word Count
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        
+        # Print character frequency
+        print("--------- Character Count -------")
+        for item in sorted_chars:
+            char = item["char"]
+            count = item["num"]
+            if char.isalpha():
+                print(f"{char}: {count}")
+        
+        # Print End
+        print("============= END ===============")
 main() 
